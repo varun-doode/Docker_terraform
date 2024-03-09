1aa80983b179416f2ad597a5c5dd61b763b7648e
varun-doode <90227231+varun-doode@users.noreply.github.com>
2024-03-09 21:28:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,22 @@ provider "aws" {
   secret_key = "uyruvmIl95bKEXQNld9UCurzkTTXibVI5YNNcC0K"
 }
 
-resource "aws_instance" "myec2" {
-    ami = "ami-0f5daaa3a7fb3378b"
-    instance_type = "t2.micro"
+resource "aws_instance" "example" {
+  ami           = "ami-0f5daaa3a7fb3378b" 
+  instance_type = "t2.micro"  
+
+  tags = {
+    Name = "assignment_instance"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+              add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              apt-get update
+              apt-get install -y docker-ce docker-ce-cli containerd.io
+              EOF
 }
+
